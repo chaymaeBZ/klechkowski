4f8e5a9bab30c7694c48b06bfc0865d44624efad
chaymaeBZ <bazzaoui.chaymae@gmail.com>
2016-07-19 12:32:41 +0100

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718184909) do
+ActiveRecord::Schema.define(version: 20160718210200) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   limit: 255
+    t.string   "name",       limit: 255
     t.string   "email",      limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,14 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+  	@user = User.new(name: "a"*50, email: "chamchem@gmail.com")
+  end
+  test "name should be valid" do
+  	assert @user.valid?
+  end
+  test "email should be valid" do
+  	@user.email = "chamoumaa"
+  	assert @user.valid?
+  end
 end
